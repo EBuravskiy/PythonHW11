103b08da177ad3051db1aa3ef28a3f19f391ac75
EBuravskiy <eburavskiy81@mail.ru>
2023-07-20 10:47:31 +0500

    Update 11.HWLesson11_1.py

--- 11.HWLesson11_1.py
+++ 11.HWLesson11_1.py
@@ -1,16 +1,32 @@
-number = int(
-    input("Enter a natural integer whose factorial you want to calculate: "))
-list1 = list()
+def calculate(n):
+    result = factorial(n)
+    factorial_list.clear()
+    result = factorial(result)
+
+
+def factorial(n):
+    result = 0
+    if n == 0:
+        result = n
+        factorial_list.insert(0, result)
+        return n
+    elif n == 1:
+        result = n
+        factorial_list.insert(0, result)
+        return n
+    result = n*(factorial(n-1))
+    factorial_list.insert(0, result)
+    return result
+
+
+number = float(
+    input("Enter a natural integer whose you want to calculate: "))
+factorial_list = list()
 if (number < 0):
     print("Error! A negative number has been entered.")
 elif (number % 1 > 0):
     print("Error! Not an integer entered.")
 else:
-    factorial = 1
-    list1.append(factorial)
-    for i in range(2, number + 1):
-        factorial *= i
-        list1.insert(0, factorial)
-print(f"The factorial of {number} is {factorial}")
-print("The resulting list of calculations:")
-print(list1)
+    number = int(number)
+    calculate(number)
+    print(factorial_list)
